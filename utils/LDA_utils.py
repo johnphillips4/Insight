73edc772b2b1109f9fb0bf59a7a0ca3f9b61e30b
John Phillips <johnphillips.400@gmail.com>
2018-12-11 18:07:47 -0500

    Add files via upload

--- utils/LDA_utils.py
+++ utils/LDA_utils.py
@@ -17,8 +17,11 @@ from gensim.parsing.preprocessing import remove_stopwords
 import pyLDAvis
 import warnings
 
+stop = ['the','a','and','an','but','to','is','of','you','are','i','for',
+    'to','in','as','as','if','work','with','it','was','on',
+    'good','great','(',')','!',"'s",'people','company','k','"','``',"''","n't",'#','&','%',"'",";",'/','\\']
 
-def format topicality(topic_list,all_topics):
+def format_topicality(topic_list,all_topics):
     """
     Formats topic list
 
@@ -37,18 +40,36 @@ def format topicality(topic_list,all_topics):
 
 
 def clean_up(review):
-	'''
-	Many reviews have sentence structure where sentences are connected with no punctuation, e.g.
-	"Hello worldHello world" This code will separate this into "hello world hello world"
-	'''
-    tokenable = re.sub('([A-Za-z][A-Z][a-z])',clean_multisentence_camel_case,review).lower()
+    '''
+    Many reviews have sentence structure where sentences are connected with no punctuation, e.g.
+    "Hello worldHello world" This code will separate this into "hello world hello world"
+    '''
+    
+    tokenable = re.sub('([A-Za-z][A-Z][a-z])',clean_multisentence_camel_case_,review).lower()
     return tokenable
 
 
+def LDA_from_df(df,num_topics=4,alpha='auto'):
+    flat_reviews = []
+    words = []
+    for review in df['PROs'].values:
+        cleaned_text = re.sub('([A-Za-z][A-Z][a-z])',clean_multisentence_camel_case_,review).lower()
+        tokens = re.sub('(\.|\,|\d|-|\\\\)',' ',cleaned_text)
+        tokens = remove_stopwords(tokens)
+        tokens = nltk.tokenize.word_tokenize(tokens)
+        tokens = [i for i in tokens if i not in stop]
+        flat_reviews.append(tokens)
+    d = Dictionary(flat_reviews)
+    c = [d.doc2bow(text) for text in flat_reviews]
+    lda = LdaModel(c, num_topics=num_topics,alpha=alpha)
+    lda.save('tmp/lda')
+    np.save('tmp/d.npy',d)
+    return lda,d
+
 def clean_multisentence_camel_case_(bit_of_string):
-	'''
-	Function for use in clean up
-	'''
+    '''
+    Function for use in clean up
+    '''
     j=1
     return bit_of_string.group(0)[:j]+' '+bit_of_string.group(0)[j:]
 
